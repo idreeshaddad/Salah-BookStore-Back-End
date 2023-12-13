8f19a8858e0b4a7ec61814e1131fd55f9d11ba9f
Idrees Haddad <gelnox@live.com>
2023-12-13 21:25:45 +0300

    Added Book dtos, IBookAppService, and BookAppService

---
+++ src/Acme.BookStore.Application/Books/BookAutoMapperProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.BookStore.Books
+{
+    public class BookAutoMapperProfile : Profile
+    {
+        public BookAutoMapperProfile()
+        {
+            CreateMap<Book, BookDto>();
+            CreateMap<CreateUpdateBookDto, Book>();
+        }
+    }
+}
