c7a7c688e22895725bb3d1549313e8af25614fc9
Idrees Haddad <gelnox@live.com>
2023-12-17 21:42:29 +0300

    - Added book permissions - Added Author domain

--- src/Acme.BookStore.Application.Contracts/Permissions/BookStorePermissions.cs
+++ src/Acme.BookStore.Application.Contracts/Permissions/BookStorePermissions.cs
@@ -4,6 +4,11 @@ public static class BookStorePermissions
 {
     public const string GroupName = "BookStore";
 
-    //Add your own permission names. Example:
-    //public const string MyPermission1 = GroupName + ".MyPermission1";
+    public static class Books
+    {
+        public const string Default = GroupName + ".Books"; // BookStore.Books (List, Details)
+        public const string Create = Default + ".Create";   // BookStore.Books.Create
+        public const string Edit = Default + ".Edit";       // BookStore.Books.Edit
+        public const string Delete = Default + ".Delete";   // BookStore.Books.Delete
+    }
 }
---
+++ src/Acme.BookStore.Domain/Authors/Author.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace Acme.BookStore.Authors
+{
+    public  class Author : FullAuditedAggregateRoot<Guid>
+    {
+        public string Name { get; private set; }
+        public DateTime BirthDate { get; set; }
+        public string ShortBio { get; set; }
+    }
+}
